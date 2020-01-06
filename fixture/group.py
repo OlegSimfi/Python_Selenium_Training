2dc0ff24e30cd186d816453efd2f574e07972ef7
Oleh <olegsimfi@gmail.com>
2020-01-06 12:22:18 +0200

    Add test_delete_some_group

--- fixture/group.py
+++ fixture/group.py
@@ -33,10 +33,22 @@ class GroupHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def delete_group_by_index(self, index):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
