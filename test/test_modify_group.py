35fc6933fe2fa52f2811de497832a207b723a495
Oleh <olegsimfi@gmail.com>
2020-01-24 21:18:34 +0200

    Add test_db_matches_ui.py

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,7 +9,7 @@ def test_modify_group_name(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
     old_groups[0] = group
-    assert sorted(old_groups, key=Group.id_or_mag) == sorted(new_groups, key=Group.id_or_mag)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
